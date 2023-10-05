3f0dec4eefa64742b756b01d87af8e1c6eb0c245
FourtyThree43 <shaqmwa@outlook.com>
2023-10-05 22:19:23 +0300

    Duck typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" Module that takes a list input and returns the first element """
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Function that takes a list input and returns the first element """
+    if lst:
+        return lst[0]
+    else:
+        return None
