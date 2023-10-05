eec1124d6dba97ccf9056e38e8a619cf4ae22056
FourtyThree43 <shaqmwa@outlook.com>
2023-10-05 23:03:15 +0300

    Mypy: Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" Module for storing the zoom_array function definition. """
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
