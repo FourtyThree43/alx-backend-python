0363ac010375d8768b19ea7b01be66469528b4b6
FourtyThree43 <shaqmwa@outlook.com>
2023-10-05 23:05:57 +0300

    pydoc

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Returns a list with all values multiplied by a factor. """
     zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
 
