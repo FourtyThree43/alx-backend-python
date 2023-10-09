0818bc7a7112b0cc24c7d368d193e58c484025ab
FourtyThree43 <shaqmwa@outlook.com>
2023-10-09 20:41:46 +0300

    added type-annotations.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ Module to run multiple coroutines at the same time with async"""
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Function that returns the list of all the delays (float values).
         The list of the delays should be in ascending order without
         using sort() because of concurrency.
     """
-    list_delays = []
+    list_delays: List[float] = []
     list_delays = [await wait_random(max_delay) for _ in range(n)]
     return sorted(list_delays)
