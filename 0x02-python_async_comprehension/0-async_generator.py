0eac06dec9f75d81ab4c4b994b936498e1e3a84c
FourtyThree43 <shaqmwa@outlook.com>
2023-10-10 16:20:53 +0300

    Automate Accounting

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Module for async generator """
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Async generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
