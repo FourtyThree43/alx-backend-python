7a9fd86b82906e50753ce96e16ee3d9d118e54ce
FourtyThree43 <shaqmwa@outlook.com>
2023-10-10 16:54:41 +0300

    Easteregg

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ Module 1-async_comprehension.py """
-import asyncio
-from typing import Any, AsyncGenerator, List
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" Module 2-measure_runtime """
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ measure_runtime function """
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = asyncio.get_event_loop().time()
+
+    total_time = end_time - start_time
+
+    return total_time
